e3bb5c618cd34325fd1f5715b47a013bd28c9fea
wccXpfhc <89910124+MesakAnggaW@users.noreply.github.com>
2022-12-18 06:37:25 +0700

    Mengerjakan Tugas 14 Javascript

---
+++ tugas14.js
@@ -0,0 +1,8 @@
+let angka = 37;
+let tglLahir = 19;
+let pembagian = angka / tglLahir;
+
+console.log("Hasil pembagian", pembagian)
+console.log("Hasil round", Math.round(pembagian));
+console.log("Hasil ceil", Math.ceil(pembagian));
+console.log("Hasil floor", Math.floor(pembagian));
